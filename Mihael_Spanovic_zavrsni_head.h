69036c88d2750cabd21d43a9fb9f1e6e80dd61b7
Mihael Španović <span.mihael@gmail.com>
2020-06-03 10:37:33 +0000

    Upload New File

---
+++ Mihael_Spanovic_zavrsni_head.h
@@ -0,0 +1,93 @@
+﻿#ifndef Mihael_Spanovic_zavrsni_head
+#define Mihael_Spanovic_zavrsni_head
+
+
+typedef struct 
+{
+	int id;
+	char ime[30];
+	char clock[7];
+	char brojjezgri[4];
+	char brojtredova[4];
+    float cijena;
+
+}PROCESOR;
+
+typedef struct
+{
+	int id;
+	char ime[30];
+	char chipset[7];
+	float cijena;
+
+}MATICNA;
+
+typedef struct
+{
+	int id;
+	char ime[30];
+	char size[6];
+	char brzina[8];
+	float cijena;
+}RAM;
+
+typedef struct
+{
+	int id;
+	char ime[30];
+	char ram[5];
+	float cijena;
+
+}GPU;
+
+typedef struct
+{
+	int id;
+	char ime[30];
+	char output[6];
+	float cijena;
+
+}NAPAJANJE;
+
+typedef struct
+{
+	int id;
+	char ime[30];
+	float cijena;
+
+}KUCISTE;
+
+typedef struct 
+{
+	PROCESOR procesor;
+	MATICNA maticna;
+	RAM ram;
+	GPU graficka;
+	NAPAJANJE psu;
+	KUCISTE kuciste;
+
+}RACUNALO;
+
+RACUNALO racunalo;
+
+
+
+void izbornik();
+void adminLogin();
+void buyerLogin();
+void adminMenu();
+void unosKomponenti();
+void brisanjeKomponenti();
+void slaganjeRacunala();
+void ispisKomponenti();
+int alocMem(int);
+
+int srcproc();
+void srcmat(int);
+void srcRAM();
+void srcgpu();
+void srcpsu();
+void srccase();
+
+
+#endif
